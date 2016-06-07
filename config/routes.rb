4bf46be9c126c8bdebeae813333579809c080642
BigBoss <pacatung@gmail.com>
2016-06-08 00:59:48 +0800

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
      resources :posts
      resources :photos
-     # resources :users
+     resources :users
 
   # Example resource route with options:
   #   resources :products do
