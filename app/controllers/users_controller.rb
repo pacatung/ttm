90473815a2784582ac1baf8f9f0fef29b4b29bdf
BigBoss <pacatung@gmail.com>
2016-06-13 18:21:42 +0800

    modify user/show html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+   before_action :find_user, :only=>[:show]
+
   def show
     @user = current_user
   end
+
+private
+  def find_user
+    @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit( :email, :image)
+  end
+
 end
