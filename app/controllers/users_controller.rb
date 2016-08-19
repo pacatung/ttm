f6808a9f57e96c4ef341d831138efbcc8c2817da
BigBoss <pacatung@gmail.com>
2016-08-19 21:12:46 +0800

    modify user show and navbar html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-   # before_action :set_user, :only=>[:show]
+   before_action :set_user, :only=>[:show]
 
   def show
-    @user = User.find( params[:id] )
+    # @user = User.find( params[:id] )
     @posts = @user.posts
   end
 
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   end
 
 private
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit( :email, :image)
   end
