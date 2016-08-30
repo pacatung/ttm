4d9e42986dcc5f66f5386afbb95d509264993bef
BigBoss <pacatung@gmail.com>
2016-08-30 12:20:12 +0800

    modify html and set access permission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,15 @@ class UsersController < ApplicationController
    before_action :set_user, :only=>[:show]
 
   def show
-    # @user = User.find( params[:id] )
-    @posts = @user.posts
-  end
-
-  def trashcan
-    # @trash_posts = Post.where(:user_id=>current_user.id and :status=>'trashcan')
-    @trash_posts = Post.where(:user_id=>current_user.id)
-
+    @posts = @user.posts.order('id desc')
   end
 
 private
   def set_user
     @user = User.find(params[:id])
   end
+
   def user_params
     params.require(:user).permit( :email, :image)
   end
-
 end
