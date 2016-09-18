18b69c4115d8e4f3f23c28356f622fe44ab3972e
BigBoss <BigBoss@Chia-Shengteki-MacBook-Air.local>
2016-09-18 18:09:17 +0800

    modify table/index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def country_flag(country_name, width)
-		image_tag("/flag/65x65/flag_#{country_name}.png", style: :"width:#{width}%;display:inline;" )
+		image_tag("/flag/65x65/flag_#{country_name}.png", style: :"width:#{width};display:inline;" )
 	end
 end
