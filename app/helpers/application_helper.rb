0184c1759406929162293c9cccba0071f9a0ddf1
BigBoss <BigBoss@Chia-Shengteki-MacBook-Air.local>
2016-10-14 16:00:18 +0800

    modify user/show, signin, signup html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def country_flag(country_name, width)
-		image_tag("/flag/65x65/flag_#{country_name}.png", style: :"width:#{width};display:inline;" )
+		image_tag("/flag/65x65/flag_#{country_name}.png", style: :"width:#{width};display:inline;margin-bottom:3px;" )
 	end
 	def setup_post(post)
 		post.countries.build if post.countries.blank?
